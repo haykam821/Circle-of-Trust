b0e2a45efac30bd85aa967a72044a5655288fbb7
haykam821 <haykam821@users.noreply.github.com>
2018-04-02 17:01:48 -0400

    Actually reject on an error

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const request = require("request-promise-native");
 
 module.exports = ownerUsername => {
-    return new Promise(async resolve => {
+    return new Promise(async (resolve, reject) => {
         const body = await request({
             method: "GET",
             url: `https://www.reddit.com/user/${ownerUsername}/circle.json`,
+        }).catch(error => {
+            reject(error);
         });
         const parsed = JSON.parse(body);
         const data = parsed[0].data.children[0].data;
