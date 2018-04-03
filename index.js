24ad95f87d1bd5195e88f8003e25e4d8e076be9b
haykam821 <haykam821@users.noreply.github.com>
2018-04-03 09:56:03 -0400

    Circles are a new class returned by the function

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const request = require("request-promise-native");
 
+class Circle {
+    constructor(data) {
+        this.name = data.title;
+        this.link = data.url;
+        this.id = data.id;
+        this.owner = data.author;
+        this.betrayed = data.is_betrayed;
+        this.score = data.score;
+        this.key = data.vote_key;
+        this.members = data.score;
+    }
+}
+
 module.exports = ownerUsername => {
     return new Promise(async (resolve, reject) => {
         const body = await request({
@@ -11,15 +24,6 @@ module.exports = ownerUsername => {
         const parsed = JSON.parse(body);
         const data = parsed[0].data.children[0].data;
 
-        resolve({
-            name: data.title,
-            link: data.url,
-            id: data.id,
-            owner: data.author,
-            betrayed: data.is_betrayed,
-            score: data.score,
-            key: data.vote_key,
-            members: data.score,
-        });
+        resolve(new Circle(data));
     });
 };
