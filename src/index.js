c8b17f6914103fcede748eac898898d39a471f33
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-01-19 17:04:44 -0500

    Simplify fetch circle function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const request = require("request-promise-native");
 const RedditCircle = require("./reddit-circle.js");
 
-module.exports = ownerUsername => {
-	return new Promise(async (resolve, reject) => {
-		const body = await request({
-			method: "GET",
-			url: `https://www.reddit.com/user/${ownerUsername}/circle.json`,
-		}).catch(error => {
-			reject(error);
-		});
-		const parsed = JSON.parse(body);
-		const data = parsed[0].data.children[0].data;
-
-		resolve(new RedditCircle(data));
+async function fetchCircle(ownerUsername) {
+	const body = await request({
+		method: "GET",
+		url: `https://www.reddit.com/user/${ownerUsername}/circle.json`,
 	});
-};
+	const parsed = JSON.parse(body);
+	const data = parsed[0].data.children[0].data;
+
+	return new RedditCircle(data);
+}
+module.exports = fetchCircle;
